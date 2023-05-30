49820b5edec1fcef3a337c124ed941aba15ada91
Andreea <andelenasamur@gmail.com>
2023-05-30 14:11:12 +0200

    making firebase work on android and web platforms

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:331329193611:web:9d5d87b1652a00b199768e";
+  static String apiKey = "AIzaSyCmTbsdUyXgXz0wU1GN_FLmlK3YfmGPKbQ";
+  static String messagingSenderId = "331329193611";
+  static String projectId = "chatappflutter-63589";
+}
