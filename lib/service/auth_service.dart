ac6978f3e656bb1352b28b13f20d0e7a3d2635b4
Andreea <andelenasamur@gmail.com>
2023-05-31 17:48:06 +0200

    authentication coding

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp_firebase/helper/helper_function.dart';
 import 'package:chatapp_firebase/service/database_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -5,6 +6,19 @@ class AuthService {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
   // login
+  Future loginWithuserNameAndPassword(String email, String password) async {
+    try {
+      User user = (await firebaseAuth.signInWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+
+      if (user != null) {
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
 
   // register
   Future registerUserWithEmailandPassword(
@@ -16,7 +30,7 @@ class AuthService {
 
       if (user != null) {
         // call our database service to update the user data.
-        await DatabaseService(uid: user.uid).updateUserData(fullname, email);
+        await DatabaseService(uid: user.uid).savingUserData(fullname, email);
 
         return true;
       }
@@ -26,4 +40,14 @@ class AuthService {
   }
 
   // signout
+  Future signOut() async {
+    try {
+      await HelperFunctions.saveUserLoggedInStatus(false);
+      await HelperFunctions.saveUserEmailSF("");
+      await HelperFunctions.saveUserNameSF("");
+      await firebaseAuth.signOut();
+    } catch (e) {
+      return null;
+    }
+  }
 }
