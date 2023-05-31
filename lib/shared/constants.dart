d5162b8943ca94b47836eb8dbfb3825a21b80c92
Andreea <andelenasamur@gmail.com>
2023-05-31 10:34:32 +0200

    create login page - UI design

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/material.dart';
 
 class Constants {
   static String? appId = dotenv.env['appId'];
   static String? apiKey = dotenv.env['apiKey'];
   static String? messagingSenderId = dotenv.env['messagingSenderId'];
   static String? projectId = dotenv.env['projectId'];
+  final primaryColor = const Color(0xFFee7b64);
 }
